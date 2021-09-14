5043b3f2d1d9ea0e3016e8c2515c135a3a4fa2b3
Blaire <76199258+Taerin-Kim@users.noreply.github.com>
2021-09-15 00:22:29 +0900

    fibonacci

--- selfHomework/src/selfHomework/fibo.java
+++ selfHomework/src/selfHomework/fibo.java
@@ -3,6 +3,18 @@ package selfHomework;
 public class fibo {
 
 	public static void main(String[] args) {
+		//피보나치수열
+		// 0 1 시작
+		// 입력 : 5
+		// 0 1
+		// 0 1 2
+		// 0 1 2 3
+		// 0 1 2 3 5
+		// 0 1 2 3 5 8
+		// List<Integer> list = new ArrayList<>();
+		// list.push(값);
+		
+		//피보나치수열에서의 주의할 점은
 		
 
 	}
