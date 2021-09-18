870d5b1af8f89e14ba87bbbb4d2a980166cb7d4a
Blaire <76199258+Taerin-Kim@users.noreply.github.com>
2021-09-18 16:34:11 +0900

    2번

--- selfHomework/src/selfHomework/star2.java
+++ selfHomework/src/selfHomework/star2.java
@@ -15,7 +15,17 @@ public class star2 {
 		*
 		
 		*/
-		
+
+		// for (int i = 0; i < 5; i++) {
+		for (int i = 5; i > 0; i--) {
+			// 별갯수
+			for (int j = i; j > 0; j--) {
+				// for(int j = i; j < 5; j++) {
+				// for (int j = i; j < 5; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 	}
 
 }
