cb8c375e7d25beccde6e061a428b8770561e5295
Blaire <76199258+Taerin-Kim@users.noreply.github.com>
2021-09-18 17:24:10 +0900

    별4
    혼자푼것

--- selfHomework/src/selfHomework/star4.java
+++ selfHomework/src/selfHomework/star4.java
@@ -3,8 +3,31 @@ package selfHomework;
 public class star4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		
+		/*
+		 
+	*****
+	 ****
+	  ***
+	   **
+	    *	 
+		 
+		 */
+		
+		//줄 수
+		for (int i = 1; i <= 5; i++) {
+			//빈칸개수
+			for (int k = i-1 ; k > 0; k--) {
+				System.out.print("1");
+			}
+			//별개수
+			for (int j = 5; j-i >= 0; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
