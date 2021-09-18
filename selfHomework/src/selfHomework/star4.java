24967b30846ad78fea680d55e07b218969578418
Blaire <76199258+Taerin-Kim@users.noreply.github.com>
2021-09-18 17:33:56 +0900

    별4
    if 문까지 합쳐서

--- selfHomework/src/selfHomework/star4.java
+++ selfHomework/src/selfHomework/star4.java
@@ -22,12 +22,39 @@ public class star4 {
 				System.out.print("1");
 			}
 			//별개수
-			for (int j = 5; j-i >= 0; j--) {
+			for (int j = 5; j-i > 0; j--) {
 				System.out.print("*");
 			}
 			System.out.println();
 		}
 		
+		System.out.println("----------------------------------");
+		//더 간단하게
+		for (int i = 1; i <= 5; i++) {
+			//빈칸개수  (0~4칸)
+			for (int k = 1; k < i; k++) {
+				System.out.print("1");
+			}
+			//별개수 (5칸 ~ 1칸)
+			for (int j = 5; j >= i; j--) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("----------------------------------");
+		//더 더 간단하게
+		for (int i = 1; i <= 5; i++) {
+			//빈칸개수  (0~4칸)
+			for (int k = 1; k <= 5; k++) {
+				if (k <= i-1) {
+					System.out.print("1");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println();
+		}
 	}
 
 }
