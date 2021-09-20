5a890c1d459ee10c3f89dcfb73759b87c5593d1a
Blaire <76199258+Taerin-Kim@users.noreply.github.com>
2021-09-20 23:47:07 +0900

    피보나치

--- selfHomework/src/selfHomework/fibo.java
+++ selfHomework/src/selfHomework/fibo.java
@@ -1,5 +1,7 @@
 package selfHomework;
 
+import java.util.Scanner;
+
 public class fibo {
 
 	public static void main(String[] args) {
@@ -12,9 +14,21 @@ public class fibo {
 		// 0 1 2 3 5
 		// 0 1 2 3 5 8
 		// List<Integer> list = new ArrayList<>();
-		// list.push(값);
+		// list.push(값); - 명령어가 pushㄱㅏ아니라 다른거 add로 추가
+		
+		//출력 받은 값으로 시작 (몇 줄을 출력 받을 건지)
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		//빈칸도 포함 (문자열처럼)
 		
 		//피보나치수열에서의 주의할 점은
+		//피보나치 수열은 재귀의 기초임
+		
+		//i는 반복 횟수 첫줄이 나와야 하니까 i값을 1로 잡아줌
+		for(int i = 1; i==n; i++) {
+			
+		}
 		
 
 	}
