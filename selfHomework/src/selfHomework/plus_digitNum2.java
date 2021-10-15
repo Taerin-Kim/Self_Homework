f6654e69819fe5adcc4f57f33715e85e248d9a8f
Blaire <76199258+Taerin-Kim@users.noreply.github.com>
2021-10-15 16:35:55 +0900

    while문 사용

--- selfHomework/src/selfHomework/plus_digitNum2.java
+++ selfHomework/src/selfHomework/plus_digitNum2.java
@@ -1,12 +1,25 @@
 package selfHomework;
 
+import java.util.Scanner;
+
 public class plus_digitNum2 {
 
 	public static void main(String[] args) {
-
 		
-		
+		System.out.println("자연수를 입력해주세요");
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		solution(input);
+		System.out.println(solution(input));
+	}
 
+	public static int solution(int N) {
+		int answer = 0;
+		while (N >= 1) {
+			answer += N % 10;
+			N /= 10;
+		}
+		return answer;
 	}
 
 }
